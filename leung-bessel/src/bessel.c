ac83fe074497ea46ae8dd68e671582d2d5d96181
Peter Williams <peter@newton.cx>
2017-09-10 13:29:55 -0400

    bessel: general punctuation tidying

--- leung-bessel/src/bessel.c
+++ leung-bessel/src/bessel.c
@@ -121,7 +121,7 @@ BesselJ_Meissel_Second(const double n, const double x)
 
 
 // Parameter for lines that deliminate between various approximations to J_n(x)
-//  ( values found empirically for n = 100..1e7 )
+//  (values found empirically for n = 100..1e7)
 #define SLOPE1 (-6.627757624078600696e-01)
 #define SLOPE2 (-6.656260931106707801e-01)
 #define SLOPE3 (-6.543033585865805080e-01)
@@ -148,23 +148,23 @@ BesselJ_Meissel_Second(const double n, const double x)
   //#endif
 
 ******************************************************************************************/
-double my_Bessel_dJ( double n, double x)
+double my_Bessel_dJ(double n, double x)
 {
   double jnp1;
-  double my_Bessel_J( double n, double x ) ;
+  double my_Bessel_J(double n, double x);
   double bessel_func;
 
-  bessel_func  = my_Bessel_J(   n,   x );
-  jnp1 = my_Bessel_J( (n+1), x );
+  bessel_func  = my_Bessel_J(n,   x);
+  jnp1 = my_Bessel_J((n+1), x);
 
   // **** problem: how about if n is between 0 and 1?
   if(x == 0.) {
     if(n >= 2.) return(0.); /* J_n(0) = 0 for n >= 1, then the recursive relation gives
                              * a zero derivative for n >= 2 */
     if(n == 0.) return(-jnp1); /* d(J_0(z))/dz = -J_1(z) */
-    return( (n*bessel_func)/(x+DBL_MIN) - jnp1 );
+    return((n*bessel_func)/(x+DBL_MIN) - jnp1);
   }
-  return( n*(bessel_func)/x - jnp1 );
+  return(n*(bessel_func)/x - jnp1);
 }
 
 /******************************************************************************************/
@@ -172,19 +172,19 @@ double my_Bessel_dJ( double n, double x)
    my_Bessel_J():
    ----------------
        -- requires x > 0, and  n > 0;
-       -- only recommend for  n > 100  ,  use glibc function jn() otherwise ;
-       -- returns approximate value of the Bessel function "J_n(x)" ;
+       -- only recommend for  n > 100  ,  use glibc function jn() otherwise;
+       -- returns approximate value of the Bessel function "J_n(x)";
        -- approximations based on those given in Chishtie et al. 2005
 ******************************************************************************************/
-double my_Bessel_J( double n, double x )
+double my_Bessel_J(double n, double x)
 {
   double fn, y, logn;
-  double BesselJ_Debye_Eps_Exp(  double n, double x ) ;
-  double BesselJ_Meissel_First(  double n, double x ) ;
+  double BesselJ_Debye_Eps_Exp(double n, double x);
+  double BesselJ_Meissel_First(double n, double x);
 
 //#if FLAG_N_JN == JN_C_LIB
   /* At least for GNU C Library, jn(n,x) requires n to be integer. */
- if( n < N_JN ) {
+ if(n < N_JN) {
     return(gsl_sf_bessel_Jn((int)n,x));
   }
 //#elif FLAG_N_JN == JN_C_LIB_interpolate
@@ -195,15 +195,15 @@ double my_Bessel_J( double n, double x )
    *  interpolation is used to calculate J_n. */
   /* If we just return jn(n,x), the bessel function at small n is a step function with
    *  jumps at every integer. The integrand of n integration becomes spiky for n < N_JN */
-//  if( n < N_JN ) {
+//  if(n < N_JN) {
 //    ni = (int)n;
 //    dn = n - ni;
 
-//    return( (1.-dn)*jn(ni,x) + dn*jn(ni+1,x) );
+//    return((1.-dn)*jn(ni,x) + dn*jn(ni+1,x));
 //  }
 //#elif FLAG_N_JN == JN_GSL
-//  if( n < N_JN ) {
-//    return( gsl_sf_bessel_Jnu(n,x) );
+//  if(n < N_JN) {
+//    return(gsl_sf_bessel_Jnu(n,x));
 //  }
 //#elif FLAG_N_JN == JN_Chishtie
 //#else
@@ -213,27 +213,27 @@ double my_Bessel_J( double n, double x )
   fn = n;
   logn = log10(fn);
 
-  if( x < fn )  {
-    y = log10( (fn - x)/fn );
+  if(x < fn)  {
+    y = log10((fn - x)/fn);
 
-    if( x!=fn ) {
+    if(x!=fn) {
       // May be useful to speed things up :
-//      if( y > (SLOPE1*logn+INTERCEPT1)  ) {
-//	return( 0. );
+//      if(y > (SLOPE1*logn+INTERCEPT1)) {
+//	return(0.);
 //      }
-      if( y > (SLOPE2*logn+INTERCEPT2)  ) {
-	return( BesselJ_Meissel_First( n , x ) );
+      if(y > (SLOPE2*logn+INTERCEPT2)) {
+	return(BesselJ_Meissel_First(n , x));
       }
     }
-    return( BesselJ_Debye_Eps_Exp( n , x ) );
+    return(BesselJ_Debye_Eps_Exp(n , x));
   }
   else {
-    y = log10( (x - fn)/x );
+    y = log10((x - fn)/x);
 
-    if( (x!=fn) && (y > (SLOPE3*logn+INTERCEPT3))  ) {
-      return( BesselJ_Meissel_Second( n , x ) );
+    if((x!=fn) && (y > (SLOPE3*logn+INTERCEPT3))) {
+      return(BesselJ_Meissel_Second(n , x));
     }
-    return( BesselJ_Debye_Eps_Exp( n , x ) );
+    return(BesselJ_Debye_Eps_Exp(n , x));
   }
 
 
@@ -251,30 +251,30 @@ double my_Bessel_J( double n, double x )
 
  ******************************************************************************************/
 
-double BesselJ_Debye_Eps_Exp( double n , double x )
+double BesselJ_Debye_Eps_Exp(double n , double x)
 {
 
-  static int  first_time = 1 ;
+  static int  first_time = 1;
   double z, ez;
   double t3, t4, t10, t38, t44, t70, t93, t107, t114, t146,t149;
   static double At[BESSEL_EPSILON_ORDER];
-  void  set_At( double At[BESSEL_EPSILON_ORDER] );
+  void  set_At(double At[BESSEL_EPSILON_ORDER]);
 
-  if( first_time ) {
+  if(first_time) {
     set_At(At);
     first_time = 0;
   }
 
-  if( x > 1.e55 ) {
-    return( 0. );
+  if(x > 1.e55) {
+    return(0.);
   }
 
   ez = x - n;
-  z = pow( x, (1./3.) );
+  z = pow(x, (1./3.));
 
   t3 = z * z;
   //t4 =  t3 * t3;   // z^4 = x^(4/3)
-  t4 =  x * z ;      // z^4 = x^(4/3)
+  t4 =  x * z;      // z^4 = x^(4/3)
   t10 = t4 *  t4;    // z^8 = x^(8/3)
   t38 = 810485676000000 * At[4] * t3;
   //t44 = t3 * z;
@@ -284,9 +284,9 @@ double BesselJ_Debye_Eps_Exp( double n , double x )
   t107 = At[13] * t3;
   t114 =  ez * ez;
   t146 = t10 * t10;
-  t149 = ((-5403237840000 * At[6] + (69470200800000 * At[4] + 19451656224000000 * At[0] * t4) * t3) * t10 * z + (-401283384 * At[15] + (4707059994 * At[13] + (3012121710 * At[12] + (-36011689560 * At[10] + (8027667648000 * At[9] + (-8027667648000 * At[7] + (-1296777081600000 * At[3] + 19451656224000000 * At[1] * t3) * t4) * t3) * z) * t3) * z) * t3 + ((-41423013450 * At[12] + (484040056500 * At[10] + (67540473000000 * At[6] - t38) * t4) * t3) * t44 + (1748257220 * At[15] + (-19964735910 * At[13] + (-2594411820000 * At[9] + (29331862560000 * At[7] + 3241942704000000 * At[3] * t4) * t3) * t4) * t3 + ((78248884350 * At[12] + (-860873013000 * At[10] + (-94556662200000 * At[6] + t38) * t4) * t3) * t44 + (-1938419560 * At[15] + (20997160275 * At[13] + (2283511230000 * At[9] - 21612951360000 * t70) * t4) * t3 + ((-47153256150 * At[12] + (459918459000 * At[10] + 27016189200000 * At[6] * t4) * t3) * t44 + (849093050 * At[15] + (-8397889500 * At[13] + (-643242600000 * At[9] + 3859455600000 * t70) * t4) * t3 + ((11448186750 * At[12] - 88445857500 * t93) * t44 + (-173573400 * At[15] + (1474097625 * At[13] + 53603550000 * At[9] * t4) * t3 + ((-1161410250 * At[12] + 5360355000 * t93) * t44 + (-113704500 * t107 + 17481100 * At[15] + (40608750 * At[12] * t44 + (-833000 * At[15] + 3123750 * t107 + 14875 * At[15] * t114) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) / ( M_PI * t146 * 0.58354968672000000e17 );
+  t149 = ((-5403237840000 * At[6] + (69470200800000 * At[4] + 19451656224000000 * At[0] * t4) * t3) * t10 * z + (-401283384 * At[15] + (4707059994 * At[13] + (3012121710 * At[12] + (-36011689560 * At[10] + (8027667648000 * At[9] + (-8027667648000 * At[7] + (-1296777081600000 * At[3] + 19451656224000000 * At[1] * t3) * t4) * t3) * z) * t3) * z) * t3 + ((-41423013450 * At[12] + (484040056500 * At[10] + (67540473000000 * At[6] - t38) * t4) * t3) * t44 + (1748257220 * At[15] + (-19964735910 * At[13] + (-2594411820000 * At[9] + (29331862560000 * At[7] + 3241942704000000 * At[3] * t4) * t3) * t4) * t3 + ((78248884350 * At[12] + (-860873013000 * At[10] + (-94556662200000 * At[6] + t38) * t4) * t3) * t44 + (-1938419560 * At[15] + (20997160275 * At[13] + (2283511230000 * At[9] - 21612951360000 * t70) * t4) * t3 + ((-47153256150 * At[12] + (459918459000 * At[10] + 27016189200000 * At[6] * t4) * t3) * t44 + (849093050 * At[15] + (-8397889500 * At[13] + (-643242600000 * At[9] + 3859455600000 * t70) * t4) * t3 + ((11448186750 * At[12] - 88445857500 * t93) * t44 + (-173573400 * At[15] + (1474097625 * At[13] + 53603550000 * At[9] * t4) * t3 + ((-1161410250 * At[12] + 5360355000 * t93) * t44 + (-113704500 * t107 + 17481100 * At[15] + (40608750 * At[12] * t44 + (-833000 * At[15] + 3123750 * t107 + 14875 * At[15] * t114) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) * ez) / (M_PI * t146 * 0.58354968672000000e17);
 
-  return( t149 ) ;
+  return(t149);
 
 
 }
@@ -298,14 +298,14 @@ double BesselJ_Debye_Eps_Exp( double n , double x )
  --------
     -- utility routine to set constant array used by   BesselJ_Debye_Eps_Exp():
  ******************************************************************************************/
-void  set_At( double At[BESSEL_EPSILON_ORDER] )
+void  set_At(double At[BESSEL_EPSILON_ORDER])
 {
-  int m ;
+  int m;
   double M;
 
-  for( m = 0 ; m < BESSEL_EPSILON_ORDER ; m++) {
+  for(m = 0; m < BESSEL_EPSILON_ORDER; m++) {
     M = (m+1)/3.;
-    At[m] = sin(M_PI*M) * pow(6., M) * exp(lgamma(M)) ;
+    At[m] = sin(M_PI*M) * pow(6., M) * exp(lgamma(M));
   }
   return;
 }
@@ -318,12 +318,12 @@ void  set_At( double At[BESSEL_EPSILON_ORDER] )
    -- good for  x<<n
 ******************************************************************************************/
 
-double BesselJ_Meissel_First( double n, double x )
+double BesselJ_Meissel_First(double n, double x)
 {
   double U, Z, eps, ninv, fn, z, Vsum1, Vsum2, exp_val, factor, exp2, invZp1;
   double t1, t2, t3, retval, loggamma_exp;
 
-  fn =  n ;
+  fn =  n;
   z = x/fn;
   eps = (fn-x)/fn;
   Z = sqrt(eps*(1+z));
@@ -341,28 +341,28 @@ double BesselJ_Meissel_First( double n, double x )
   Vsum2 = -(ninv * (420 + (-14 + (-4 + 3 * t2) * t2) * t2)) / 0.5040e4;
 
   // I substitute  Gamma(n+1) with (n+1)*Gamma(n)  in the denominator:
-  factor  = 1./( (fn+1.) * sqrt(Z) );
+  factor  = 1./((fn+1.) * sqrt(Z));
 
 
   // 1/(1+Z) to eigth order
-  if( Z < 1.e-3 ) {
+  if(Z < 1.e-3) {
     invZp1 = 1 + (-1 + (1 + (-1 + (1 + (-1 + (1 - Z) * Z) * Z) * Z) * Z) * Z) * Z;
   }
   else {
     invZp1 = 1./(1.+Z);
   }
 
-  if( (eps < 1.e-4) && (fn > 1.e3)  ) {
+  if((eps < 1.e-4) && (fn > 1.e3)) {
     t3 = t2*t2;
     loggamma_exp =  (ninv * (-420 + 14 * t2 - 4 * t3 + 3 * t3 * t2)) / 0.5040e4;  // O(1/n^k) part of -log(gamma(n))
     exp2 = -fn * sqrt(2.*eps) * eps * (0.984023040e9 + (0.442810368e9 + (0.303114240e9 + (0.233192960e9 + (0.190139040e9 + (0.160692840e9 + 0.139204065e9 * eps) * eps) * eps) * eps) * eps) * eps) / 0.1476034560e10;
     exp_val = 0.5*log(0.5*fn/M_PI) + loggamma_exp + exp2 - Vsum1 - Vsum2;
   }
   else {
-    exp_val = n*(  log(x*invZp1) - (1 - Z) ) - Vsum1 - Vsum2 - lgamma(n) ;
+    exp_val = n*(log(x*invZp1) - (1 - Z)) - Vsum1 - Vsum2 - lgamma(n);
   }
 
-  retval = exp_factor( factor, exp_val ) ;
+  retval = exp_factor(factor, exp_val);
 
-  return( retval ) ;
+  return(retval);
 }
