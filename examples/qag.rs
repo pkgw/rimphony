7e41cf69b3ffa14148b899a147d66e4faa6d202d
Peter Williams <peter@newton.cx>
2017-09-10 18:57:13 -0400

    examples: some more integration tests
    Numerical integration, that is.

--- examples/qag.rs
+++ examples/qag.rs
@@ -1,13 +1,22 @@
 extern crate rimphony;
 
 use rimphony::gsl;
+use std::f64;
 
-fn main() {
+fn inner(i: i32) {
     let mut ws = gsl::IntegrationWorkspace::new(1024);
 
-    let r = ws.qagiu(|x| { 1. / (x * x) }, 0.5)
+    let closure = |x: f64| { x.cos().powi(i) };
+
+    println!("Sample: {}", closure(0.));
+
+    let r = ws.qag(closure, 0., 0.5 * f64::consts::PI)
         .tolerance(0., 1e-6)
         .compute();
 
     println!("Result: {}   Abs err: {}", r.value, r.abserr);
+}
+
+fn main() {
+    inner(2);
 }
