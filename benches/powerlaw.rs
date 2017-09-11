632440512627d56d58316c9312b568a5c8ce687f
Peter Williams <peter@newton.cx>
2017-09-11 14:33:15 -0400

    Add a benchmark program [benchdata]
    Current results on my laptop:
    
    ```
    test powerlaw_ai_0 ... bench: 364,935,649 ns/iter (+/- 296,490)
    test powerlaw_ai_4 ... bench: 264,408,717 ns/iter (+/- 128,353)
    test powerlaw_aq_0 ... bench: 409,582,568 ns/iter (+/- 877,000)
    test powerlaw_aq_1 ... bench: 217,537,239 ns/iter (+/- 138,524)
    test powerlaw_av_0 ... bench: 1,027,370,430 ns/iter (+/- 744,229)
    test powerlaw_av_2 ... bench: 1,395,605,358 ns/iter (+/- 6,228,579)
    test powerlaw_ji_0 ... bench: 242,558,927 ns/iter (+/- 171,604)
    test powerlaw_ji_1 ... bench: 225,710,921 ns/iter (+/- 111,151)
    test powerlaw_jq_0 ... bench: 330,355,524 ns/iter (+/- 287,959)
    test powerlaw_jq_2 ... bench: 182,634,627 ns/iter (+/- 491,351)
    test powerlaw_jv_0 ... bench: 1,043,875,058 ns/iter (+/- 486,493)
    test powerlaw_jv_3 ... bench: 1,030,021,147 ns/iter (+/- 288,924)
    ```
    
    When making changes that affect the benchmarks, put [benchdata] in the first
    line of the commit message so that it's easy to check how things evolve.

---
+++ benches/powerlaw.rs
@@ -0,0 +1,137 @@
+/// Crunch some numbers for in the powerlaw model.
+///
+/// Due to the way it’s designed, the benchmarker runs each "iteration" at
+/// least 300 times, so this program can take a while to run. It adds up to
+/// about 40 minutes on my machine.
+
+#[macro_use] extern crate bencher;
+extern crate rimphony;
+
+use rimphony::{ELECTRON_CHARGE, MASS_ELECTRON, SPEED_LIGHT, TWO_PI, Coefficient, Stokes};
+use bencher::Bencher;
+
+
+// I designed a neat little Latin square system to try to run benchmarks
+// across a moderate cross-section of typical parameters. But to keep runtimes
+// relatively reasonable, I'm mostly not using it.
+
+const NUS: &[f64] = &[3e8, 1e9, 3e9, 3e10, 3e11];
+const N_ES: &[f64] = &[1e2, 1e3, 1e4, 1e5, 1e6];
+const SS: &[f64] = &[1e0, 1e1, 1e2, 1e3, 1e4];
+const THETAS: &[f64] = &[0.05, 0.430, 0.810, 1.190, 1.5707];
+const PS: &[f64] = &[1.5, 1.75, 2.5, 3.25, 4.];
+const GAMMA_MIN: f64 = 1.;
+const GAMMA_MAX: f64 = 1000.;
+const GAMMA_CUTOFF: f64 = 1e7;
+
+const LATIN_SQUARE: &[usize] = &[
+    1, 4, 2, 3, 0,
+    3, 1, 0, 4, 2,
+    0, 3, 1, 2, 4,
+    2, 0, 4, 1, 3,
+    4, 2, 3, 0, 1,
+];
+
+fn powerlaw_inner(coeff: rimphony::Coefficient, row_number: usize) {
+    // This function used to loop over every row in the Latin square, but that
+    // would lead to the benchmark taking way too long to run.
+
+    let base = row_number * 5;
+    let nu = NUS[LATIN_SQUARE[base + 0]];
+    let n_e = N_ES[LATIN_SQUARE[base + 1]];
+    let s = SS[LATIN_SQUARE[base + 2]];
+    let theta = THETAS[LATIN_SQUARE[base + 3]];
+    let p = PS[LATIN_SQUARE[base + 4]];
+
+    let bfield = TWO_PI * MASS_ELECTRON * SPEED_LIGHT * nu / (ELECTRON_CHARGE * s);
+
+    rimphony::PowerLawDistribution::new(p)
+        .gamma_limits(GAMMA_MIN, GAMMA_MAX, GAMMA_CUTOFF)
+        .finish(coeff, nu, bfield, n_e, theta)
+        .compute();
+}
+
+
+fn powerlaw_ji_0(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Emission(Stokes::I), 0);
+    });
+}
+
+fn powerlaw_jq_0(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Emission(Stokes::Q), 0);
+    });
+}
+
+fn powerlaw_jv_0(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Emission(Stokes::V), 0);
+    });
+}
+
+fn powerlaw_ai_0(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Absorption(Stokes::I), 0);
+    });
+}
+
+fn powerlaw_aq_0(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Absorption(Stokes::Q), 0);
+    });
+}
+
+fn powerlaw_av_0(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Absorption(Stokes::V), 0);
+    });
+}
+
+
+// A smattering of other parameter configurations.
+
+fn powerlaw_ji_1(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Emission(Stokes::I), 1);
+    });
+}
+
+fn powerlaw_jq_2(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Emission(Stokes::Q), 2);
+    });
+}
+
+fn powerlaw_jv_3(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Emission(Stokes::V), 3);
+    });
+}
+
+fn powerlaw_ai_4(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Absorption(Stokes::I), 4);
+    });
+}
+
+fn powerlaw_aq_1(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Absorption(Stokes::Q), 1);
+    });
+}
+
+fn powerlaw_av_2(b: &mut Bencher) {
+    b.iter(|| {
+        powerlaw_inner(Coefficient::Absorption(Stokes::V), 2);
+    });
+}
+
+
+benchmark_group!(powerlaw_0,
+                 powerlaw_ji_0, powerlaw_jq_0, powerlaw_jv_0,
+                 powerlaw_ai_0, powerlaw_aq_0, powerlaw_av_0);
+benchmark_group!(powerlaw_others,
+                 powerlaw_ji_1, powerlaw_jq_2, powerlaw_jv_3,
+                 powerlaw_ai_4, powerlaw_aq_1, powerlaw_av_2);
+benchmark_main!(powerlaw_0, powerlaw_others);
