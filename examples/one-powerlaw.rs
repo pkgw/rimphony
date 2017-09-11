ef1a7f7a3beddffc01d877af350daf36f5fee4b9
Peter Williams <peter@newton.cx>
2017-09-10 20:10:55 -0400

    examples/one-powerlaw.rs: test case with simpler-to-type parameters

--- examples/one-powerlaw.rs
+++ examples/one-powerlaw.rs
@@ -1,22 +1,19 @@
 /// Compute one coefficient for the power-law distribution.
-///
-/// This uses the parameters from my first Symphony sample.
 
 extern crate rimphony;
 
-use rimphony::{ELECTRON_CHARGE, MASS_ELECTRON, SPEED_LIGHT, TWO_PI, Coefficient, Stokes};
+use rimphony::{Coefficient, Stokes};
 
 fn main() {
-    const S: f64 = 5.39186e+07;
-    const THETA: f64 = 1.29985;
-    const P: f64 = 1.70877;
     const NU: f64 = 1e9;
+    const B: f64 = 1e3;
     const N_E: f64 = 1.;
+    const THETA: f64 = 0.9;
+    const P: f64 = 2.5;
     const GAMMA_MIN: f64 = 1.;
     const GAMMA_MAX: f64 = 1000.;
     const GAMMA_CUTOFF: f64 = 1e7;
-    const SYMPHONY_JI: f64 = 1.63868e-31;
-    const B: f64 = TWO_PI * MASS_ELECTRON * SPEED_LIGHT * NU / (ELECTRON_CHARGE * S);
+    const SYMPHONY_JI: f64 = 2.64399749412774e-21;
 
     let ji = rimphony::PowerLawDistribution::new(P)
         .gamma_limits(GAMMA_MIN, GAMMA_MAX, GAMMA_CUTOFF)
