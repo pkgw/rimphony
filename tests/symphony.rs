b0eaa1f32e5f488082cab0e07ad534906d31996f
Peter Williams <peter@newton.cx>
2017-09-11 08:48:14 -0400

    tests/symphony.rs: also test absorption coefficients

--- tests/symphony.rs
+++ tests/symphony.rs
@@ -10,14 +10,13 @@ use std::path::PathBuf;
 //use std::str::StrExt;
 
 const TOP: &'static str = env!("CARGO_MANIFEST_DIR");
-const REL_ERR_THRESHOLD: f64 = 1e-4;
 
 /// These computations were parameterized by the harmonic number *s*, observer
 /// angle *theta*, and power-law index *p*. Other parameters were chosen
 /// fiducially; the coefficients can be scaled deterministically to apply for
 /// any values of *B*, *nu*, and *n_e*.
 
-fn compare_powerlaw_subset(coeff: rimphony::Coefficient, cname: &str, index: usize, fraction_to_check: f64) {
+fn compare_powerlaw_subset(coeff: rimphony::Coefficient, cname: &str, index: usize, fraction_to_check: f64, rtol: f64) {
     const NU: f64 = 1e9;
     const N_E: f64 = 1.;
     const GAMMA_MIN: f64 = 1.;
@@ -48,7 +47,7 @@ fn compare_powerlaw_subset(coeff: rimphony::Coefficient, cname: &str, index: usi
         let theirs = v[index];
         let rel_err = ((ours - theirs) / theirs).abs();
 
-        if rel_err > REL_ERR_THRESHOLD {
+        if rel_err > rtol {
             panic!("disagree with Symphony for {}; they have {:.6e}, we have {:.6e}; at parameters:
 
 s: {:.6e}
@@ -60,15 +59,32 @@ p: {:.6e}", cname, theirs, ours, s, theta, p);
 
 #[test]
 fn compare_powerlaw_subset_ji() {
-    compare_powerlaw_subset(Coefficient::Emission(Stokes::I), "J_I", 3, 0.0002);
+    compare_powerlaw_subset(Coefficient::Emission(Stokes::I), "J_I", 3, 0.002, 1e-4);
 }
 
 #[test]
 fn compare_powerlaw_subset_jq() {
-    compare_powerlaw_subset(Coefficient::Emission(Stokes::Q), "J_Q", 5, 0.0002);
+    compare_powerlaw_subset(Coefficient::Emission(Stokes::Q), "J_Q", 5, 0.002, 1e-4);
+}
+
+/// Stokes V is a bit more demanding so the error threshold is looser.
+#[test]
+fn compare_powerlaw_subset_jv() {
+    compare_powerlaw_subset(Coefficient::Emission(Stokes::V), "J_V", 7, 0.002, 1e-3);
 }
 
 #[test]
-fn compare_powerlaw_subset_jv() {
-    compare_powerlaw_subset(Coefficient::Emission(Stokes::V), "J_V", 7, 0.0002);
+fn compare_powerlaw_subset_ai() {
+    compare_powerlaw_subset(Coefficient::Absorption(Stokes::I), "A_I", 4, 0.002, 1e-4);
+}
+
+#[test]
+fn compare_powerlaw_subset_aq() {
+    compare_powerlaw_subset(Coefficient::Absorption(Stokes::Q), "A_Q", 6, 0.002, 1e-4);
+}
+
+/// Stokes V is a bit more demanding so the error threshold is looser.
+#[test]
+fn compare_powerlaw_subset_av() {
+    compare_powerlaw_subset(Coefficient::Absorption(Stokes::V), "A_V", 8, 0.002, 1e-3);
 }
